b1f1eddd125839a9d70b2efc1f28f8256429835e
Karlova Lesia <karlovalesa06@gmail.com>
2022-01-14 11:26:24 +0200

    Create auth forms validation

--- src/components/pages/RegistrationPage/RegistrationPage.js
+++ src/components/pages/RegistrationPage/RegistrationPage.js
@@ -2,23 +2,20 @@ import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useAlert } from 'react-alert';
+import { Formik, Form } from 'formik';
+import { registrationValidationScheme } from '../../../schemas/authSchemas';
 import { setUserData } from '../../../store/userSlice';
 import { registerUser } from '../../../api/auth';
 import MainContent from '../../common/MainContent';
 import Image from '../../common/Image';
-import AuthForm from '../../AuthForm';
 import Input from '../../common/Input';
+import ErrorAlert from '../../common/alerts/ErrorAlert';
 import AuthFormLink from '../../common/AuthFormLink';
 import Button from '../../common/Button';
-import ErrorAlert from '../../common/alerts/ErrorAlert/ErrorAlert';
 import RegisterImage from '../../../assets/images/register.jpg';
 import { AUTHORIZATION_PAGE, MAIN_PAGE } from '../../../constants/routes';
 
 function RegistrationPage() {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const dispatch = useDispatch();
@@ -27,59 +24,54 @@ function RegistrationPage() {
 
   const { show } = useAlert();
 
-  const onFirstNameChange = (event) => {
-    const { value } = event.target;
-    setFirstName(value);
-  };
-
-  const onLastNameChange = (event) => {
-    const { value } = event.target;
-    setLastName(value);
-  };
-
-  const onEmailChange = (event) => {
-    const { value } = event.target;
-    setEmail(value);
-  };
-
-  const onPasswordChange = (event) => {
-    const { value } = event.target;
-    setPassword(value);
-  };
-
-  const register = (event) => {
-    event.preventDefault();
-
+  const handleRegister = (userData) => {
     setIsLoading(true);
 
-    registerUser({
-      firstName, lastName, email, password,
-    }).then(({ accessToken, user }) => {
+    registerUser(userData).then(({ accessToken, user }) => {
+      setIsLoading(false);
+
       dispatch(setUserData({
         token: accessToken,
         ...user,
       }));
+
       history.push(MAIN_PAGE);
     })
-      .catch((error) => show(
-        <ErrorAlert>
-          {error.response.data}
-        </ErrorAlert>,
-      ))
-      .finally(() => setIsLoading(false));
+      .catch((error) => {
+        setIsLoading(false);
+
+        return show(
+          <ErrorAlert>
+            {error.response.data}
+          </ErrorAlert>,
+        );
+      });
   };
 
   return (
     <MainContent title="Registration">
       <Image source={RegisterImage} altText="Registration background" />
-      <AuthForm onSubmit={register}>
-        <Input label="First name" value={firstName} type="text" placeholder="First name" onChange={onFirstNameChange} />
-        <Input label="Last name" value={lastName} type="text" placeholder="Last name" onChange={onLastNameChange} />
-        <Input label="Email" value={email} type="email" placeholder="Email" onChange={onEmailChange} />
-        <Input label="Password" value={password} type="password" placeholder="Password" onChange={onPasswordChange} />
-        <AuthFormLink text="Already registered?" path={AUTHORIZATION_PAGE} link="Log in" />
-        <Button wrapperClasses="auth-btn-wrapper" type="submit" disabled={isLoading} isLoading={isLoading}>Register</Button>
-      </AuthForm>
+      <Formik
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          password: '',
+        }}
+        validationSchema={registrationValidationScheme}
+        onSubmit={handleRegister}
+      >
+        <Form className="w-400">
+          <div>
+            <Input label="First name" type="text" name="firstName" placeholder="First name" />
+            <Input label="Last name" type="text" name="lastName" placeholder="Last name" />
+            <Input label="Email" type="email" name="email" placeholder="Email" />
+            <Input label="Password" type="password" name="password" placeholder="Password" />
+            <AuthFormLink text="Already registered?" path={AUTHORIZATION_PAGE} link="Log in" />
+            <Button wrapperClasses="auth-btn-wrapper" type="submit" disabled={isLoading} isLoading={isLoading}>Register</Button>
+          </div>
+        </Form>
+      </Formik>
     </MainContent>
   );
 }
