5bdc8a53adac95e3acc8839178e24fd2ee89f659
Karlova Lesia <karlovalesa06@gmail.com>
2022-01-21 11:49:01 +0200

    Add PROJECTS_ROUTE and PROJECT_PAGE constants

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,5 +1,7 @@
-const MAIN_PAGE = '/';
-const REGISTRATION_PAGE = '/register';
-const AUTHORIZATION_PAGE = '/login';
+export const MAIN_PAGE = '/';
 
-export { MAIN_PAGE, REGISTRATION_PAGE, AUTHORIZATION_PAGE };
+export const REGISTRATION_PAGE = '/register';
+export const AUTHORIZATION_PAGE = '/login';
+
+export const PROJECTS_ROUTE = '/projects';
+export const PROJECT_PAGE = '/projects/:id';
