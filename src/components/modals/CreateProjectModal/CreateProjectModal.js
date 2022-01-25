a9d92bcf6d3d60a2c6be27d31a9d4feab3463965
Karlova Lesia <karlovalesa06@gmail.com>
2022-01-25 15:46:30 +0200

    Implement create project

---
+++ src/components/modals/CreateProjectModal/CreateProjectModal.js
@@ -0,0 +1,99 @@
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Formik, Form } from 'formik';
+import { createProject } from '../../../api/projects';
+import Modal from '../Modal';
+import Input from '../../common/Input';
+import Textarea from '../../common/Textarea';
+import CloseModalIcon from '../../icons/CloseModalIcon';
+import ModalCreateProjectIcon from '../../icons/ModalCreateProjectIcon';
+import Button from '../../common/Button';
+import FontIcon from '../../icons/FontIcon';
+import FontWeightIcon from '../../icons/FontWeightIcon';
+import FontItalicIcon from '../../icons/FontItalicIcon';
+import FontColorIcon from '../../icons/FontColorIcon';
+import { createProjectValidationScheme } from '../../../schemas/projectSchemas';
+import './styles.scss';
+
+function CreateProjectModal({ onClose, onCreate }) {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { id: userId } = useSelector(({ user }) => user);
+
+  const createNewProject = ({ title, description }) => {
+    setIsLoading(true);
+
+    createProject({ userId, title, description }).then(({ id }) => {
+      setIsLoading(false);
+      onCreate({
+        id, title, description, userId,
+      });
+      onClose();
+    });
+  };
+
+  return (
+    <Modal
+      headerChildren={(
+        <>
+          <h1>
+            Create
+            <br />
+            Project
+          </h1>
+          <button onClick={onClose}>
+            <CloseModalIcon />
+          </button>
+        </>
+      )}
+    >
+      <Formik
+        initialValues={{
+          title: '',
+          description: '',
+        }}
+        validationSchema={createProjectValidationScheme}
+        onSubmit={createNewProject}
+      >
+        <Form className="modal-form">
+          <Input labelValue="Name" type="text" name="title" placeholder="Name" />
+          <div className="label-wrapper">
+            <label className="label">Description</label>
+            <div className="modal-btn-group">
+              <button>
+                <FontIcon />
+              </button>
+              <button>
+                <FontWeightIcon />
+              </button>
+              <button>
+                <FontItalicIcon />
+              </button>
+              <button>
+                <FontColorIcon />
+              </button>
+            </div>
+          </div>
+          <Textarea name="description" placeholder="Description" />
+          <Button wrapperClasses="modal-btn-wrapper" type="submit" isLoading={isLoading}>
+            <ModalCreateProjectIcon />
+            Create
+          </Button>
+        </Form>
+      </Formik>
+    </Modal>
+  );
+}
+
+CreateProjectModal.propTypes = {
+  onClose: PropTypes.func,
+  onCreate: PropTypes.func,
+};
+
+CreateProjectModal.defaultProps = {
+  onClose: () => {},
+  onCreate: () => {},
+};
+
+export default CreateProjectModal;
--- src/components/pages/ProjectsPage/ProjectsPage.js
+++ src/components/pages/ProjectsPage/ProjectsPage.js
@@ -5,27 +5,52 @@ import MainContent from '../../common/MainContent';
 import ProjectsList from '../../ProjectsList';
 import Button from '../../common/Button';
 import CreateProjectIcon from '../../icons/CreateProjectIcon';
+import CreateProjectModal from '../../modals/CreateProjectModal';
 import './styles.scss';
 
 function ProjectsPage() {
   const [projects, setProjects] = useState([]);
+  const [isCreateProjectModalOpen, setIsCreateProjectModalOpen] = useState(false);
 
   const { id: userId } = useSelector(({ user }) => user);
 
   useEffect(() => getProjects({ userId }).then((response) => setProjects(response)), []);
 
+  const openCreateProjectModal = () => {
+    setIsCreateProjectModalOpen(true);
+  };
+
+  const closeCreateProjectModal = () => {
+    setIsCreateProjectModalOpen(false);
+  };
+
+  const onCreateProject = (project) => {
+    setProjects([...projects, project]);
+  };
+
   return (
-    <MainContent title="Projects">
-      <div className="projects-block-wrapper">
-        {(projects.length)
-          ? <ProjectsList projects={projects} />
-          : <span className="text">No projects yet</span>}
-        <Button>
-          <CreateProjectIcon />
-          Create
-        </Button>
-      </div>
-    </MainContent>
+    <div>
+      <MainContent title="Projects">
+        <div className="projects-block-wrapper">
+          {projects.length
+            ? <ProjectsList projects={projects} />
+            : <span className="text">No projects yet</span>}
+          <Button onClick={openCreateProjectModal}>
+            <CreateProjectIcon />
+            Create
+          </Button>
+        </div>
+      </MainContent>
+      {
+          isCreateProjectModalOpen
+          && (
+          <CreateProjectModal
+            onClose={closeCreateProjectModal}
+            onCreate={onCreateProject}
+          />
+          )
+      }
+    </div>
   );
 }
 
