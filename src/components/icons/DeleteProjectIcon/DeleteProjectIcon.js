33847217ae47fbb9fe4c9c4aac3748895cdff6c7
Karlova Lesia <karlovalesa06@gmail.com>
2022-02-07 18:13:36 +0200

    Add styles prop

--- src/components/icons/DeleteProjectIcon/DeleteProjectIcon.js
+++ src/components/icons/DeleteProjectIcon/DeleteProjectIcon.js
@@ -1,14 +1,23 @@
+import PropTypes from 'prop-types';
 import { FaTrashAlt } from 'react-icons/fa';
 import { IconContext } from 'react-icons';
 
-function DeleteProjectIcon() {
+function DeleteProjectIcon({ style }) {
   return (
-    <IconContext.Provider value={{ color: 'lightgrey', size: '16px' }}>
+    <IconContext.Provider value={style}>
       <div>
         <FaTrashAlt />
       </div>
     </IconContext.Provider>
   );
 }
+
+DeleteProjectIcon.propTypes = {
+  style: PropTypes.object,
+};
+
+DeleteProjectIcon.defaultProps = {
+  style: { color: 'lightgrey', size: '16px' },
+};
 
 export default DeleteProjectIcon;
