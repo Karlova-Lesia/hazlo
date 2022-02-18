7e4496fa0fd838d384a80fc1ae582292de606254
Karlova Lesia <karlovalesa06@gmail.com>
2022-02-18 21:06:05 +0200

    Add MembersColumn in ProjectPage

--- src/components/pages/ProjectPage/ProjectPage.js
+++ src/components/pages/ProjectPage/ProjectPage.js
@@ -3,21 +3,28 @@ import { useParams } from 'react-router-dom';
 import { getProject } from '../../../api/projects';
 import MainContent from '../../common/MainContent';
 import TaskBoard from '../../TaskBoard';
+import MembersColumn from '../../MembersColumn';
 import './styles.scss';
 
 function ProjectPage() {
-  const [project, setProject] = useState({});
+  const [project, setProject] = useState();
 
   const { id } = useParams();
 
-  useEffect(() => getProject({ id }).then((response) => setProject(response)), []);
+  useEffect(() => getProject({ id })
+    .then((response) => {
+      setProject(response);
+    }), []);
 
-  return (
-    <MainContent title={project.title} style={{ flexDirection: 'column' }}>
+  return project ? (
+    <MainContent title={project.title} style={{ flexDirection: 'column' }} className="project-main-content-wrapper">
       <p className="description-text">{project.description}</p>
-      <TaskBoard projectId={id} />
+      <div className="project-main-content">
+        <TaskBoard projectId={id} />
+        <MembersColumn project={project} />
+      </div>
     </MainContent>
-  );
+  ) : <> </>;
 }
 
 export default ProjectPage;
